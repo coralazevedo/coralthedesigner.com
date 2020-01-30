8eaec96e034e532aded009c47d8c2c56046aded4
Coral Azevedo <coralstudios123@gmail.com>
2020-01-30 12:33:45 -0500

    adding js alert

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function alertUser(msg) {
+alert(msg);
+}
 
+
+/*
+// if hover -> display txt
+function displayTxt(evt) {
+    evt.currentTarget.parentNode.querySelector( '.preview-title' ).classList.remove( 'hide' );
+}
+// if leave -> hide txt
+function removeTxt(evt) {
+    evt.currentTarget.querySelector( '.preview-title' ).classList.add( 'hide' );
+}
+
+/* mouseover and mouseout events to `.previewimage` element */
+/*var $previewimage = document.querySelector( '.previewimage' );
+$previewimage.addEventListener( 'mouseover', displayTxt );
+$rowcol.addEventListener( 'mouseout', removeTxt );
+*/
